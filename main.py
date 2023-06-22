7f4c3a9fcb66c064b03140374dab4df30d54e039
fedor-leonov <infinitefedor@gmail.com>
2023-06-22 12:03:05 -0700

    refactoring

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 import discord
-import psycopg2
 from discord.ext import commands
+import psycopg2
 import os
-from database import *
-
-#get the API key
 from dotenv import load_dotenv
-load_dotenv()
+import logging
+
+load_dotenv() #get .env file
+#initialize all .env variables
+ownerID = os.getenv('OWNERID')
+dbName = os.getenv('DBNAME')
+dbUser = os.getenv('DBUSER')
+dbPass = os.getenv('DBPASSWORD')
+key = os.getenv('KEY')
 
 #set up discord intents
 intents = discord.Intents.default()
@@ -14,16 +19,23 @@ intents.members = True
 intents.message_content = True
 intents.moderation = True
 
+#variables
+botDescription = 'Discord bot for managing PostgresSQL databases'
+error = "oopsy! FedorBot had a fucky wucky! talk to fedor!"
+logFile = 'bot.log'
+
+#enable log
+handler = logging.FileHandler(filename=logFile, encoding='utf-8', mode='w')
+
 client = discord.Client(intents=intents)
-bot = commands.Bot(command_prefix='$', description='funny little fedor bot', intents=intents)
+bot = commands.Bot(command_prefix='$', description=f'{botDescription}', intents=intents)
 
 #connect to postgres database
-conn = psycopg2.connect(f"dbname={os.getenv('DBNAME')} user={os.getenv('USER')} password={os.getenv('PASSWORD')}")
+conn = psycopg2.connect(f"dbname={dbName} user={dbUser} password={dbPass}")
 cur = conn.cursor() # create cursor
-print('Connected to database.')
 
-def isFedor(ctx):
-    if ctx.author.id == 919372259842465893:
+def ownerCheck(ctx):
+    if ctx.author.id == ownerID:
         return True
     else:
         return False
@@ -34,27 +46,39 @@ async def on_ready():
     await bot.change_presence(status=discord.Status.online, activity=discord.Game('with your mom'))
 
 @bot.command()
-async def table(ctx, *, arg):
-    await ctx.send(arg)
+async def table(ctx, *args):
+    if (len(args)/2).is_integer() != True: #check if the length of arguments is a multiple of two
+        await ctx.send(error)
+    else:
+        await ctx.send(args)
 
 @bot.command()
 async def mystery(ctx):
     print(await bot.fetch_channel(668178989583695886))
 
 @bot.command()
+async def createChannel(ctx, category, channel):
+    if ownerCheck(ctx):
+        categoryName = category
+        category = discord.utils.get(ctx.guild.categories, name=categoryName)
+        await ctx.guild.create_text_channel(f'{channel}', category=category)
+    else:
+        await ctx.send(error)
+
+@bot.command()
 async def turnoff(ctx):
-    if isFedor(ctx):
+    if ownerCheck(ctx):
         await ctx.send("shutting down database...")
         cur.close()
         conn.close()
         await ctx.send("closing bot connection...")
         await bot.close()
     else:
-        await ctx.send("oopsy! FedorBot had a fucky wucky because you're not fedor! try asking fedor to be able to do this command.")
+        await ctx.send(error)
         return
         
 
-bot.run(os.getenv('KEY'))
+bot.run(key, log_handler=handler)
 
 
 
