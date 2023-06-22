1f439733bf63b00f8fa3439c768ac794f6851932
fedor-leonov <infinitefedor@gmail.com>
2023-06-21 23:47:20 -0700

    Add psgl conn

--- main.py
+++ main.py
@@ -1,38 +1,40 @@
 import discord
+import psycopg2
 from discord.ext import commands
 import os
+from database import *
 
 #get the API key
 from dotenv import load_dotenv
 load_dotenv()
 
-
+#set up discord intents
 intents = discord.Intents.default()
 intents.members = True
 intents.message_content = True
 intents.moderation = True
 
 client = discord.Client(intents=intents)
-bot = commands.Bot(command_prefix='$', description="funny little fedor bot", intents=intents)
-    
+bot = commands.Bot(command_prefix='$', description='funny little fedor bot', intents=intents)
 
+#connect to postgres database
+conn = psycopg2.connect(f"dbname={os.getenv('DBNAME')} user={os.getenv('USER')} password={os.getenv('PASSWORD')}")
+cur = conn.cursor() # create cursor
+print('Connected to database.')
+
+def isFedor(ctx):
+    if ctx.author.id == 919372259842465893:
+        return True
+    else:
+        return False
+    
 @bot.event
 async def on_ready():
     print(f'We have logged in as {bot.user}')
-    await bot.change_presence(status=discord.Status.online, activity=discord.Game("with your mom"))
-
-@bot.event
-async def on_reaction_add(reaction, user):
-    print(reaction)
-    dmuser = await bot.fetch_user(user.id)
-    await dmuser.send(f"Do you think you're funny? I know where you live. I'll kill you. Stop adding {reaction} to my messages or else.")
-
-@bot.event
-async def on_guild_channel_create(channel):
-    print(f'{channel} has been created in {channel.category}')
+    await bot.change_presence(status=discord.Status.online, activity=discord.Game('with your mom'))
 
 @bot.command()
-async def foo(ctx, *, arg):
+async def table(ctx, *, arg):
     await ctx.send(arg)
 
 @bot.command()
@@ -41,12 +43,16 @@ async def mystery(ctx):
 
 @bot.command()
 async def turnoff(ctx):
-    if ctx.author.id != 919372259842465893:
+    if isFedor(ctx):
+        await ctx.send("shutting down database...")
+        cur.close()
+        conn.close()
+        await ctx.send("closing bot connection...")
+        await bot.close()
+    else:
         await ctx.send("oopsy! FedorBot had a fucky wucky because you're not fedor! try asking fedor to be able to do this command.")
         return
-    else:
-        await ctx.send("shutting down...")
-        await bot.close()
+        
 
 bot.run(os.getenv('KEY'))
 
