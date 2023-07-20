6491f5f90b875ebd87d72f9670d31a513f37488c
fedor-leonov <infinitefedor@gmail.com>
2023-07-20 10:28:58 -0700

    Change everything to Hypixel API

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 # FedorBot
 
-Discord bot to manage PostgresSQL databases written in Python using discord.py.
+Discord bot to view Hypixel online status.
+
+Commands:
+    $createWatchPoint USERNAME: Checks every 5 seconds whether the provided username is online on Hypixel.
+    $isOnline USERNAME: Checks whether the provided username is online on Hypixel
+    $lastOnline USERNAME: Checks the last logout date of the provided username on Hypixel. May not work with old users on Hypixel.
 
 ## Credits
 Created by Fedor Leonov (talk to me on Discord at davidperkins)
--- main.py
+++ main.py
@@ -1,20 +1,20 @@
 import discord
 from discord.ext import commands
-import psycopg2
-import os
-import asyncio
+from discord.ext import tasks
 from dotenv import load_dotenv
+from datetime import datetime
+import requests
+import asyncio
 import logging
+import os
+
 
 commandPrefix = '$'
 load_dotenv() # load dotenv
-botOwnerDiscordID = os.getenv('OWNERID')
-databaseName = os.getenv('DBNAME')
-databaseUser = os.getenv('DBUSER')
-databasePass = os.getenv('DBPASSWORD')
-discordBotkey = os.getenv('KEY')
-botDescription = 'Discord bot for managing PostgresSQL databases'
-error = "oopsy! FedorBot had a fucky wucky! talk to fedor!"
+discordBotkey = os.getenv('BOTKEY')
+hypixelKey = os.getenv('HYPIXELKEY')
+botDescription = 'Bday Gift for Dad'
+error = "Oops! Error occured!"
 logFile = 'bot.log'
 
 #set up discord intents
@@ -27,9 +27,6 @@ intents.moderation = True
 handler = logging.FileHandler(filename=logFile, encoding='utf-8', mode='w')
 client = discord.Client(intents=intents)
 bot = commands.Bot(command_prefix=f'{commandPrefix}', description=f'{botDescription}', intents=intents)
-
-def checkIfUserIsBotOwner(ctx):
-    return ctx.author.id == int(botOwnerDiscordID)
     
 @bot.event
 async def on_ready():
@@ -37,40 +34,89 @@ async def on_ready():
     await bot.change_presence(status=discord.Status.online, activity=discord.Game('with Postgres Databases'))
 
 @bot.command()
-async def foo(ctx, arg):
-    await ctx.send(*arg)
+async def lastOnline(ctx, username):
+    playerData = requests.get(
+        url = "https://api.hypixel.net/player",
+        params = {
+            "key": f"{hypixelKey}",
+            "name": f"{username}"
+        }
+    ).json()
+    if playerData["success"]:
+        if(playerData == "null"):
+            await ctx.send(error)
+        else:
+            try:
+                lastLogout = playerData["player"]["lastLogout"]
+                await ctx.send(f"{username} was last online at {datetime.fromtimestamp(lastLogout/1000)}")
+            except KeyError:
+                await ctx.send(f"Hypixel API does not have online info about {username}")
+    else:
+        await ctx.send(error + "Have you searched this name recently?")
+
+
+@bot.command()
+async def isOnline(ctx, username):
+    playerID = requests.get(
+        url = f"https://api.mojang.com/users/profiles/minecraft/{username}"
+    ).json()["id"]
+    playerData = requests.get(
+        url = "https://api.hypixel.net/status",
+        params = {
+            "key": f"{hypixelKey}",
+            "uuid": f"{playerID}"
+        }
+    ).json()
+    if playerData["success"]:
+        if playerData["session"]["online"]:
+            await ctx.send(f"{username} is currently online!")
+        else:
+                await ctx.send(f"{username} is currently not online!")
+    else:
+        await ctx.send(error + " Have you searched this name recently?")
+
+@tasks.loop(seconds=10)
+async def watchUser(ctx, username):
+    playerID = requests.get(
+        url = f"https://api.mojang.com/users/profiles/minecraft/{username}"
+    ).json()["id"]
+    playerData = requests.get(
+        url = "https://api.hypixel.net/status",
+        params = {
+            "key": f"{hypixelKey}",
+            "uuid": f"{playerID}"
+        }
+    ).json()
+    if playerData["success"]:
+        if playerData["session"]["online"]:
+            await ctx.send(f"{username} is on!")
+        else:
+            await ctx.send(f"{username} is not on!")
+    else:
+        await ctx.send(error + " Have you searched this name recently?")
+
+
+@bot.command()
+async def createWatchPoint(ctx, username):
+    watchUser(ctx, username).start()
+    
+@bot.command()
+async def createChannel(ctx, category, channel):
+    categoryName = category
+    category = discord.utils.get(ctx.guild.categories, name=categoryName)
+    await ctx.guild.create_text_channel(f'{channel}', category=category)
 
 @bot.command()
 async def createRole(ctx, roleName, perm):
-    if checkIfUserIsBotOwner(ctx):
-        author = ctx.message.author
-        await author.add_roles(await ctx.guild.create_role(name=f"{roleName}", permissions=discord.Permissions(permissions=int(perm))))
-    else:
-        await ctx.send(error)
-
+    author = ctx.message.author
+    await author.add_roles(await ctx.guild.create_role(name=f"{roleName}", permissions=discord.Permissions(permissions=int(perm))))
+    
 @bot.command()
-async def createChannel(ctx, category, channel):
-    if checkIfUserIsBotOwner(ctx):
-        categoryName = category
-        category = discord.utils.get(ctx.guild.categories, name=categoryName)
-        await ctx.guild.create_text_channel(f'{channel}', category=category)
-    else:
-        await ctx.send(error)
+async def turnOff(ctx):
+    await ctx.send("Closing bot connection...")
+    await bot.close()
 
-@bot.command()
-async def turnoff(ctx):
-    if checkIfUserIsBotOwner(ctx):
-        await ctx.send("shutting down database...")
-        cur.close()
-        conn.close()
-        await ctx.send("closing bot connection...")
-        await bot.close()
-    else:
-        await ctx.send(error)
-        return
-        
-
-bot.run(key, log_handler=handler)
+bot.run(discordBotkey, log_handler=handler)
 
 
 
